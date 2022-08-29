f932f49384a3944db3df17db5aa6b0016ea3e32d
Abin Jacob <aj4abinjacob@gmail.com>
2022-08-29 22:03:47 +0530

    finished 12

--- Class HW/variable.js
+++ Class HW/variable.js
@@ -21,3 +21,53 @@ console.log(companies);
 console.log([1, 2, 3, 4, 5, 5, 7, 5, 7, 8].lastIndexOf(5));
 
 // 8. Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'.
+console.log('You cannot end a sentence with because because because is a conjunction'.search("because"));
+// search returns position of the word
+
+// 9. Use trim() to remove any trailing whitespace at the beginning and the end of a string.
+" hello world ".trim();
+
+// 10. Boolean value is either true or false.
+//     - Write three JavaScript statement which provide truthy value.
+//     - Write three JavaScript statement which provide falsy value.
+// false
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(0));
+// all values are truthy except false, 0, -0, 0n, "", null, undefined, and NaN.
+// truth
+console.log(Boolean("a"));
+console.log(Boolean(1));
+console.log(Boolean([]));
+
+// 11. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//     - 4 > 3 true
+//     - 4 >= 3 false
+//     - 4 < 3 false
+//     - 4 <= 3 false
+//     - 4 == 4 true
+//     - 4 === 4 true
+//     - 4 != 4 false
+//     - 4 !== 4 false
+//     - 4 != '4' false
+//     - 4 == '4' true
+//     - 4 === '4' false
+// - Find the length of python and jargon and make a falsy comparison statement.
+console.log("python".length > "jargon".length);
+
+// 12. Use the Date object to do the following activities
+const current_date = new Date();
+//     - What is the year today?
+console.log(current_date.getFullYear());
+//     - What is the month today as a number?
+console.log(current_date.getMonth() + 1);
+//     - What is the date today?
+console.log(current_date.getDate());
+//     - What is the day today as a number?
+console.log(current_date.getDate());
+//     - What is the hours now?
+console.log(current_date.getHours());
+//     - What is the minutes now?
+console.log(current_date.getMinutes());
+//     - Find out the numbers of seconds elapsed from January 1, 1970 to now.
+console.log(Math.floor(current_date.getTime() / 1000));
