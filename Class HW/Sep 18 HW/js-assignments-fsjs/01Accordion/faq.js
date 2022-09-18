a527d03f86685830e7d7bf41f516a19c7c8cdae7
Abin Jacob <aj4abinjacob@gmail.com>
2022-09-18 14:55:11 +0530

    Project 1.1 done

--- Class HW/Sep 18 HW/js-assignments-fsjs/01Accordion/faq.js
+++ Class HW/Sep 18 HW/js-assignments-fsjs/01Accordion/faq.js
@@ -18,19 +18,34 @@ const faqData = [
   },
 ];
 
-const accordianBody;
+// const accordianBody;
 const faqs = [];
 
-function showFaq() {
-  
+function createFaq(data) {
+  let card_div = document.createElement('div');
+  card_div.innerHTML = `<div class="faq_header">
+                          <h3>${data['question']}</h3>
+                          <button class="show_btn"></button>
+                        </div>
+                        <p class="hidden">${data['answer']}</p>
+                        `
+  card_div.querySelector("button").style.marginLeft = "auto";
+  card_div.classList.add("faq");
+  document.querySelector(".accordian_body").appendChild(card_div);
 }
+faqData.forEach(el=>createFaq(el));
+document.querySelectorAll(".show_btn").forEach(el => {
+  el.addEventListener('click',(event) => {
+    let parent = event.currentTarget.parentNode.parentNode;
+    console.log(parent);
+    if (parent.querySelector("p").style.display === '') {
+      parent.querySelector("p").style.display = "block";
+    } else {
+      parent.querySelector("p").style.display = "";
+    }
+  })
+})
 
-function createFaq() {
-  
-}
 
-function btnStatusUpdate() {
-  
-}
 
 
