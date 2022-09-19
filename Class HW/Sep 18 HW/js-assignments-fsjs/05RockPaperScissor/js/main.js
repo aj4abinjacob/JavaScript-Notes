3e3c7d502a59e30c1364f38091c576f718378802
Abin Jacob <aj4abinjacob@gmail.com>
2022-09-19 18:49:21 +0530

    Project 1.5 done

--- Class HW/Sep 18 HW/js-assignments-fsjs/05RockPaperScissor/js/main.js
+++ Class HW/Sep 18 HW/js-assignments-fsjs/05RockPaperScissor/js/main.js
@@ -2,26 +2,56 @@ Array.prototype.random = function(){
     return this[Math.floor(Math.random() * this.length)]
 }
 
+let computer = document.getElementById("result-comp-stat");
+let user = document.getElementById("result-user-stat");
+let winnerRT = document.getElementById("result-final-stat");
+
+let userScoreVal = document.getElementById("userScoreVal");
+let computerScoreVal = document.getElementById("compScoreVal");
+
 let choices = ['rock', 'paper', 'scissors'];
 
 document.querySelectorAll(".choice").forEach((el) => {
     el.addEventListener("click",(event)=>{
         let computer_choice = choices.random();
+        computer.innerHTML = `Computer: ${computer_choice}`;
         let user_choice = event.currentTarget.querySelector("img").getAttribute("alt");
-        console.log(user_choice,computer_choice);
+        user.innerHTML = `User: ${user_choice}`;
 
         const win_cases = {
             'rock' : 'scissors',
             'paper' : 'rock',
             'scissors' : 'paper'
         }
-        
+
+        let winner;
         if (user_choice === computer_choice){
-            console.log("It's a draw");
+            winner = "Draw";
         }else if (win_cases[user_choice] === computer_choice){
-            console.log("You win")
+            winner = "User";
+            userScoreVal.innerHTML = `${Number(userScoreVal.innerHTML) + 1}`;
         }else{
-            console.log("Computer Wins")
+            winner = "Computer";
+            computerScoreVal.innerHTML = `${Number(computerScoreVal.innerHTML) + 1}`;
         }
+        winnerRT.innerHTML = `Winner: ${winner}`
+        
+        document.querySelectorAll('[id$="ScoreVal"]').forEach(el => {
+            if (Number(el.innerHTML.match(/\d+/g).join("")) === 10){
+                if(el.getAttribute("id").includes("user")){
+                    alert("User has won the match");
+                }else{
+                    alert("Computer has won the match");
+                }
+                userScoreVal.innerHTML = 0;
+                computerScoreVal.innerHTML = 0;
+                winnerRT.innerHTML = `Winner: `;
+                user.innerHTML = `User: `;
+                computer.innerHTML = `Computer: `;
+            }
+        })
     })
 })
+
+
+
