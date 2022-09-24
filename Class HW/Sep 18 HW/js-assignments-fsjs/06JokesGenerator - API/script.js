5e902644c322c05ae2cd05355832f05030d37fa7
Abin Jacob <aj4abinjacob@gmail.com>
2022-09-24 15:27:49 +0530

    Added working api

--- Class HW/Sep 18 HW/js-assignments-fsjs/06JokesGenerator - API/script.js
+++ Class HW/Sep 18 HW/js-assignments-fsjs/06JokesGenerator - API/script.js
@@ -2,10 +2,14 @@ let url = "https://v2.jokeapi.dev/joke/Dark?type=twopart"
 async function fetchJokes(){
 
     const joke = await (await fetch(url)).json();
-
+    
     return joke;
+
 }
 
-document.getElementById("jokeBtn").addEventListener("click",(()=>{
-    
+document.getElementById("jokeBtn").addEventListener("click",async ()=>{
+ 
+    const joke = await fetchJokes();
+    // document.querySelector("#joke").innerHTML = joke;
+    document.querySelector("#joke").innerHTML = `${joke['setup']}<br/>${joke['delivery']}`;
 })
