2e81db9497f2b4f256623c81d78a06e9b76c3ad6
Abin Jacob <aj4abinjacob@gmail.com>
2022-09-27 22:56:09 +0530

    palindrome done

--- Class HW/Sep 18 HW/Projects/Palindrome Checker/script.js
+++ Class HW/Sep 18 HW/Projects/Palindrome Checker/script.js
@@ -6,12 +6,12 @@ btn.addEventListener('click', () => {
     const text = input_text.value.toLowerCase()
     if (text === text.split('').reverse().join('')) {
         card.style.backgroundColor = '#1FAA59';
-        document.querySelector("p").innerHTML = 'The text you have entered is Palindrome'
+        document.querySelector("p").innerHTML = 'The text you have entered is Palindrome.'
         document.querySelector(".fa-face-frown").style.display = 'none';
         document.querySelector(".fa-face-smile").style.display = 'block';
     } else {
         card.style.backgroundColor = '#EF5354';
-        document.querySelector("p").innerHTML = 'The text you have entered is not Palindrome'
+        document.querySelector("p").innerHTML = 'The text you have entered is not Palindrome.'
         document.querySelector(".fa-face-smile").style.display = 'none';
         document.querySelector(".fa-face-frown").style.display = 'block';
     }
