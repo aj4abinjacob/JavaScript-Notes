55900b573cb17a5e279d9a27dddb0cf70f71eec0
Abin Jacob <aj4abinjacob@gmail.com>
2022-09-27 22:59:56 +0530

    added check for empty text

--- Class HW/Sep 18 HW/Projects/Palindrome Checker/script.js
+++ Class HW/Sep 18 HW/Projects/Palindrome Checker/script.js
@@ -4,7 +4,13 @@ let input_text = document.querySelector("input");
 const card = document.querySelector(".card");
 btn.addEventListener('click', () => {
     const text = input_text.value.toLowerCase()
-    if (text === text.split('').reverse().join('')) {
+    if (text.replace(' ', '').length === 0) {
+        card.style.backgroundColor = '#E8BD0D';
+        document.querySelector("p").innerHTML = 'Please enter proper text.'
+        document.querySelector(".fa-face-smile").style.display = 'none';
+        document.querySelector(".fa-face-frown").style.display = 'block';
+    }
+    else if (text === text.split('').reverse().join('')) {
         card.style.backgroundColor = '#1FAA59';
         document.querySelector("p").innerHTML = 'The text you have entered is Palindrome.'
         document.querySelector(".fa-face-frown").style.display = 'none';
