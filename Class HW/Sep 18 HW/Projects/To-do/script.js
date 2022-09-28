e85e681b9d2681d909de20c4a550851e76f213a8
Abin Jacob <aj4abinjacob@gmail.com>
2022-09-28 22:34:10 +0530

    added working model for adding deleting tasks

--- Class HW/Sep 18 HW/Projects/To-do/script.js
+++ Class HW/Sep 18 HW/Projects/To-do/script.js
@@ -1,25 +1,30 @@
+const card_container = document.querySelector(".cards-container")
 const to_do_array = [];
-
+const input_text = document.querySelector("input");
 
 function rmParent(el) {
     el.parentElement.remove();
 }
 
 function createCard() {
+    const text = input_text.value;
     const mini_container = document.createElement('div');
     mini_container.classList.add('mini-container');
     const rm_btn = document.createElement('div');
-    rm_btn.innerHTML = `<i class="fa-solid fa-check"></i>`
-    // <i class="fa-solid fa-minus"></i>
-    mini_container.classList.add('rm');
+    rm_btn.innerHTML = `<div class="rm">
+                            <i class="fa-solid fa-minus"></i>
+                        </div>
+                        `
+    rm_btn.setAttribute('onclick', `rmParent(this)`)
+
     const card = document.createElement('div');
     card.classList.add('card');
+    card.innerHTML = `<p>${text}</p>`
     mini_container.appendChild(card);
     mini_container.appendChild(rm_btn);
-
+    card_container.appendChild(mini_container)
 }
 
-const card_container = document.querySelector(".card-container")
 
 document.querySelector('#add').addEventListener('click', () => {
     createCard();
